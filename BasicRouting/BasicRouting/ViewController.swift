19fc13c49293ca2f6b0e58fb7ba6a7df1087bf7b
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-07-19 16:49:06 +0100

    push all

--- BasicRouting/BasicRouting/ViewController.swift
+++ BasicRouting/BasicRouting/ViewController.swift
@@ -192,3 +192,37 @@ private extension ViewController {
         )
     }
 }
+
+//
+//class ViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        if SYContext.isInitialized() {
+//            setupMapView()
+//        } else {
+//            let center = NotificationCenter.default
+//            var token: NSObjectProtocol?
+//            token = center.addObserver(forName: .DidInitSygicMaps, object: nil, queue: nil) { [weak self] (_) in
+//
+//                center.removeObserver(token!)
+//                self?.setupMapView()
+//            }
+//        }
+//    }
+//
+//    func setupMapView() {
+//        let mapView = SYMapView(frame: view.bounds)
+//
+//        mapView.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(mapView)
+//
+//        NSLayoutConstraint.activate([
+//            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+//            view.bottomAnchor.constraint(equalTo: mapView.bottomAnchor),
+//            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            view.trailingAnchor.constraint(equalTo: mapView.trailingAnchor)
+//        ])
+//    }
+//}
